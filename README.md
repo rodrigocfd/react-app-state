e3fb9f22e145afc3170946a85c4d7a4a3c0346d1
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2017-10-01 15:32:46 -0300

    Simpler design, easier to extend.

--- README.md
+++ README.md
@@ -2,21 +2,32 @@
 
 A simple state manager for React.
 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Extending AppState](#extending-appstate)
+* [Computed values](#computed-values)
+* [Multiple subscriptions](#multiple-subscriptions)
+* [Full API documentation](#full-api-documentation)
+
 ## Installation
+
+AppState is intended to be 100% compatible with [create-react-app](https://github.com/facebookincubator/create-react-app#readme). No need to "eject" or install anything but AppState itself:
 
     npm install --save react-app-state
 
 ## Usage
 
-Instantiate a state manager:
+Instantiate an AppState manager and initialize the variables it will keep:
 
+    // --- fooState.js ---
     import AppState from 'react-app-state';
 
-    let fooState = new AppState();
+    let fooState = new AppState({ myName: '' });
     export default fooState;
 
-If a component wants to update the state, it calls `set` method passing the variables to be changed:
+If a component wants to update the state, it calls `set` method passing the variables to be updated:
 
+    // --- InputComponent.js ---
     import React from 'react';
     import fooState from './fooState';
 
@@ -25,51 +36,49 @@ If a component wants to update the state, it calls `set` method passing the vari
         Name:
         <input type="text"
           onChange={ev =>
-            fooState.set({
-              myName: ev.target.value
-            })
-          }>
+            fooState.set({ myName: ev.target.value })
+          }/>
       </div>
     );
 
     export default InputComponent;
 
-If a component wants to consume the state, it subscribes to the variables it wants to receive by calling `subscribe`, which returns a [HOC](https://facebook.github.io/react/docs/higher-order-components.html). The subscribed variables come via `props`:
+If a component wants to consume the state, it calls `subscribe`, which returns a [HOC](https://facebook.github.io/react/docs/higher-order-components.html). The subscribed variables then come through `props`:
 
+    // --- DisplayComponent.js ---
     import React from 'react';
     import fooState from './fooState';
 
-    const DisplayComponent = (props) => (
+    const DisplayComponent = ({ myName }) => (
       <div>
-        My name is {props.myName}.
+        My name is {myName}.
       </div>
     );
 
-    export default fooState.subscribe(DisplayComponent, 'myName');
+    export default fooState.subscribe(DisplayComponent);
 
-## With a wrapper
+## Extending AppState
 
-We can have an ordinary class to take care of the `set` calls, maybe enforcing a business rule:
+Extending AppState is useful to enforce business logic, or just to add a layer over the raw stored values.
 
+    // --- nameOwner.js ---
     import AppState from 'react-app-state';
 
-    class NameOwner {
+    class NameOwner extends AppState {
       constructor() {
-        this.ourState = new AppState({ myName: 'Sr. Doe' });
-        // here we defined an initial value, this is optional
+        super({ myName: 'Mr. Doe' });
       }
+
       chooseMyName(name) {
-        this.ourState.set({ myName: 'Sr. ' + name });
-      }
-      subscribe(TheComponent) {
-        return this.ourState.subscribe(TheComponent, 'myName');
+        super.set({ myName: 'Mr. ' + name });
       }
     }
 
     export default new NameOwner();
 
-And now we update the state like this:
+Updating the state:
 
+    // --- InputComponent.js ---
     import React from 'react';
     import nameOwner from './nameOwner';
 
@@ -85,19 +94,75 @@ And now we update the state like this:
 
     export default InputComponent;
 
-And consume...
+And consuming:
 
+    // --- DisplayComponent.js ---
     import React from 'react';
     import nameOwner from './nameOwner';
 
-    const DisplayComponent = (props) => (
+    const DisplayComponent = ({ myName }) => (
       <div>
-        My name is {props.myName}.
+        My name is {myName}.
       </div>
     );
 
     export default nameOwner.subscribe(DisplayComponent);
 
-And there's also a `get` method to retrieve a variable imperatively, if you ever need:
+## Computed values
 
-    let name = nameOwner.get('myName');
+Extending AppState is also useful to provide computed values. Taking the previous example, we could choose to store `myName` as typed by the user and have a method to return the computed string:
+
+    // --- nameOwner.js ----
+    import AppState from 'react-app-state';
+
+    class NameOwner extends AppState {
+      constructor() {
+        super({ myName: '' });
+      }
+
+      get prettyName() { // a getter, could be an ordinary function too
+        return 'Mr. ' + super.get('myName');
+      }
+
+      chooseMyName(name) { // could also be a setter
+        super.set({ myName: name });
+      }
+    }
+
+    export default new NameOwner();
+
+Consuming the computed value:
+
+    // --- DisplayComponent.js ---
+    import React from 'react';
+    import nameOwner from './nameOwner';
+
+    const DisplayComponent = () => (
+      <div>
+        My name is {nameOwner.prettyName}.
+      </div>
+    );
+
+    export default nameOwner.subscribe(DisplayComponent);
+
+## Multiple subscriptions
+
+A component can subscribe to any number of AppState managers. Since `subscribe` returns a component, just chain the calls:
+
+    nameOwner.subscribe(
+      anotherState.subscribe(
+        yetAnotherManager.subscribe(DisplayComponent)
+      )
+    );
+
+The component will receive the variables of all those managers via `props`. Just beware name collisions: if two variables have the same name, the last one will prevail.
+
+## Full API documentation
+
+`AppState(propsAndValues)` – constructor. Instantiates AppState and initializes the values to be kept.
+
+`AppState.subscribe(WrappedComponent)` – subscribes a component to the state manager instance. The component will receive the variables through its `props`. A component can subscribe to any number of AppState instances.
+
+`AppState.get(propName)` – retrieves a value imperatively. Usually you don't need this, most of the time `props` is enough.
+
+`AppState.set(propsAndValues, callback)` – updates the internal state, propagating the new values to the subscribed components. The 1st argument contains the values to be updated. The 2nd argument is a callback to be called after the state is set, just like an ordinary `setState` call works.
--- src/AppState.js
+++ src/AppState.js
@@ -5,130 +5,71 @@
  * @see https://github.com/rodrigocfd/react-app-state
  */
 
-import 'babel-polyfill';
 import React from 'react';
 
-/* INTERNAL STRUCTURE LOOKS LIKE THIS:
-this._variables = {
-	"variable": { // a variable being subscribed to
-		components: {
-			"componentId": { // a component which subscribed to this variable
-				instances: {
-					"instanceId": AppStateSubscriber // an instance that must receive props
-				}
-			}
-		},
-		value: { ... } // current value for this variable
-	],
-	"anotherVariable": { ... }
-};
-*/
-
 export default class AppState {
-	// Instantiates a new class, optionally setting initial values.
-	// new AppState({ myName:'foo' });
-	constructor(initialState) {
-		this._nextComponentId = 0; // these are supposed to be private...
-		this._variables = { };     // ...if you mess with them, you're looking for trouble
-
-		if (initialState) {
-			this.set(initialState);
-		}
+	constructor(propsAndValues) {
+		this._propsAndValues = propsAndValues; // each prop with initial value
+		this._components = []; // each subscribed component
+		this._instanceCount = 0; // how many instances to call setState()
 	}
 
-	// Updates one or more values.
-	// set({ myName:'foo', mySurname:'bar' }, () => alert('done!'));
-	set(userVars, callback) {
-		let uniqueComponents = { }; // components that subscribed to a variable that user is updating
-		let uniqueStates = { }; // unique state values to send to each component
-		let instanceCount = 0; // how many instances are to be notified summing all components
-		let subscriptionsHappened = (Object.keys(this._variables).length > 0); // component parsing/subscription already happened?
-
-		for (const userVar of Object.keys(userVars)) { // each variable the user wants to update
-			let variable = this._variables[userVar]; // a subscribed variable
-			if (!variable) {
-				if (subscriptionsHappened) {
-					throw new Error('Updating a variable that no component has subscribed to: ' + userVar);
-				} else { // probably an initial value being set in a controller class
-					variable = { components: { } };
-					this._variables[userVar] = variable; // create new entry
-				}
-			}
-			variable.value = userVars[userVar]; // store new value for variable
-
-			for (const componentId of Object.keys(variable.components)) { // components subscribed to this variable
-				if (!uniqueComponents[componentId]) { // not added yet?
-					uniqueComponents[componentId] = variable.components[componentId];
-					uniqueStates[componentId] = { }; // will be passed to setState()
-					instanceCount += Object.keys(uniqueComponents[componentId].instances).length;
-				}
-				uniqueStates[componentId][userVar] = userVars[userVar]; // add the variable that will be sent to instance
-			}
-		}
-
-		let asyncDispatch = () => {
-			if (callback && --instanceCount === 0) {
-				callback(); // user callback is fired after the last setState() returns
-			}
-		};
-
-		for (const componentId of Object.keys(uniqueComponents)) {
-			let compToGo = uniqueComponents[componentId];
-			let stateToGo = uniqueStates[componentId];
-			for (const instanceId of Object.keys(compToGo.instances)) {
-				compToGo.instances[instanceId].setState(stateToGo, // notify each instance with variables it subscribed to
-					asyncDispatch); // callback asynchronous behavior just like setState()
-			}
-		}
-	}
-
-	// Retrieve one value imperatively.
-	// get('myName');
-	get(userVar) {
-		return this._variables[userVar] ?
-			this._variables[userVar].value : undefined;
-	}
-
-	// Subscribes a component to one or more variables, returns a HOC.
-	// subscribe(MyComponent, 'myName', 'mySurname')
-	subscribe(WrappedComponent, ...subscribedVars) {
-		let componentId = 'co' + (this._nextComponentId++);
+	subscribe(WrappedComponent) {
 		let component = {
-			instances: { },
+			instances: { }, // AppStateSubscriber instances
 			nextInstanceId: 0
 		};
-
-		for (const subsVar of subscribedVars) {
-			if (!this._variables[subsVar]) { // no component subscribed to this variable yet?
-				this._variables[subsVar] = { components: { } }; // create new entry
-			}
-			this._variables[subsVar].components[componentId] = component; // store component using componentId as key
-		}
-
-		let allVariables = this._variables; // so we can access this._variables inside AppStateSubscriber class below
+		this._components.push(component);
+		let THIS = this; // so we can access it inside class below
 
 		return class AppStateSubscriber extends React.Component {
 			constructor(props) {
 				super(props);
 				this._instanceId = 'in' + (component.nextInstanceId++);
-				component.instances[this._instanceId] = this; // store instance using instanceId as key
-				let stateValues = { };
-
-				for (const subsVar of subscribedVars) {
-					if (allVariables[subsVar].value) { // a set() was already called on this variable?
-						stateValues[subsVar] = allVariables[subsVar].value; // instance receives saved value right away
-					}
-				}
-				this.state = stateValues; // will be passed to child props in render()
+				component.instances[this._instanceId] = this; // store instance using ID as key
+				++THIS._instanceCount;
+				this.state = { ...THIS._propsAndValues }; // receive current values right away
 			}
 
 			componentWillUnmount() {
-				delete component.instances[this._instanceId]; // so set() won't dispatch to an inexistent object
+				--THIS._instanceCount;
+				delete component.instances[this._instanceId]; // so set() won't dispatch to inexistent instance
 			}
 
 			render() {
-				let allProps = { ...this.props, ...this.state }; // set() triggers this.state change, we add into wrapped's props
-				return <div><WrappedComponent {...allProps}/></div>;
+				return <WrappedComponent {...this.props} {...this.state}/>;
+			}
+		}
+	}
+
+	get(propName) {
+		if (!this._propsAndValues.hasOwnProperty(propName)) {
+			throw new Error('Retrieving an inexistent prop: ' + propName);
+		}
+		return this._propsAndValues[propName];
+	}
+
+	set(propsAndValues, callback) {
+		for (const propName of Object.keys(propsAndValues)) {
+			if (!this._propsAndValues.hasOwnProperty(propName)) {
+				throw new Error('Updating an inexistent prop: ' + propName);
+			}
+		}
+		this._propsAndValues = { ...this._propsAndValues, ...propsAndValues }; // update internal store
+
+		let asyncCallback = null;
+		if (callback) {
+			let instanceCount = this._instanceCount;
+			asyncCallback = () => {
+				if (--instanceCount === 0) {
+					callback(); // user callback is fired after last setState() returns
+				}
+			};
+		}
+
+		for (const component of this._components) {
+			for (const instanceId of Object.keys(component.instances)) {
+				component.instances[instanceId].setState({ ...propsAndValues }, asyncCallback); // AppStateSubscriber.setState()
 			}
 		}
 	}
